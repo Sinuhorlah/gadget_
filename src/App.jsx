64be0154cff0144650d1edb6268022b2e577a6a3
Sinuhorlah <akinrinolaabisolaa@gmail.com>
2025-01-22 17:37:40 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Body from "./assets/Components/Body";
 import Footer from "./assets/Components/Footer";
 import Hero from "./assets/Components/Hero";
 import Navbar from "./assets/Components/Navbar";
+// import Navbar from "./assets/Components/Navbar";
 import Products from "./assets/Components/Products";
 function App() {
  
